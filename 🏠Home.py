d218cd0bdca5934a42d121ef7791d4988d41e4f2
Theophilus Nathan Tanudjaja <114735443+Theophilus03@users.noreply.github.com>
2025-05-23 15:23:12 +0700

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -14,6 +14,75 @@ st.set_page_config(
     page_icon="assets/logo.png"
 )
 
+
+with st.spinner("Wait for it...", show_time=True):
+    #dataset
+    if 'data' not in st.session_state:
+      df = pd.read_csv('assets/final.csv')
+      df_clean = df.copy()
+      df_clean = df_clean.replace(['-', 'TIDAK ADA DATA', '---'], np.nan)
+      df_clean = df_clean[['pm10', 'so2', 'co', 'o3', 'no2', 'kategori']]
+    
+      for col in ['pm10', 'so2', 'co', 'o3', 'no2']:
+          df_clean[col] = pd.to_numeric(df_clean[col], errors='coerce')
+      df_clean = df_clean.dropna()
+      st.session_state.data = df_clean
+      X = df_clean.drop('kategori', axis=1)
+      y = df_clean['kategori']
+    
+      #dataset mapping
+      ordinal_mapping = {
+          'BAIK': 0,
+          'SEDANG': 1,
+          'TIDAK SEHAT': 2,
+          'SANGAT TIDAK SEHAT': 3
+      }
+    
+      y_encoded = y.map(ordinal_mapping)
+    
+      #data splitting
+      X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, random_state=42, stratify=y)
+      st.session_state.X_train = X_train
+      st.session_state.X_test = X_test
+      st.session_state.y_train = y_train
+      st.session_state.y_test = y_test
+    
+    #ordinal logistic regression
+    if 'ordinal_logistic' not in st.session_state:
+      from statsmodels.miscmodels.ordinal_model import OrderedModel
+      mod_log = OrderedModel(st.session_state.y_train,
+                            st.session_state.X_train,
+                            distr='logit')
+      res_log = mod_log.fit(method='bfgs', disp=False)
+      st.session_state.ordinal_logistic = res_log
+    
+    
+    #XGBoost
+    if 'xgboost' not in st.session_state:
+      import xgboost as xgb
+      xgboost = xgb.XGBClassifier(
+          objective='multi:softmax',
+          num_class=4,
+          max_depth=25,
+          learning_rate=0.001,
+          n_estimators=100,
+      )
+      xgboost.fit(st.session_state.X_train, st.session_state.y_train)
+      st.write('selesai trainning')
+      st.session_state.xgboost = xgboost
+      st.write('dah simpen')
+        
+      #tabnet
+    if 'tabnet' not in st.session_state:
+      from pytorch_tabnet.tab_model import TabNetClassifier
+      tabnet = TabNetClassifier()
+      tabnet.load_model('assets/full_tabnet_model.pth.zip')
+      st.session_state.tabnet = tabnet
+
+
+
+
+
 icon_url = f"data:image/png;base64,{image_to_base64('assets/logo.png')}"
 title_text = "AirCheck"
 
@@ -34,67 +103,4 @@ st.markdown(
      Kami menggunakan berbagai model statistik dan machine learning untuk menganalisis kualitas udara dan memprediksi perubahan kualitas udara di masa depan berdasarkan data polutan.
     """
 )
-
-#dataset
-if 'data' not in st.session_state:
-  df = pd.read_csv('assets/final.csv')
-  df_clean = df.copy()
-  df_clean = df_clean.replace(['-', 'TIDAK ADA DATA', '---'], np.nan)
-  df_clean = df_clean[['pm10', 'so2', 'co', 'o3', 'no2', 'kategori']]
-
-  for col in ['pm10', 'so2', 'co', 'o3', 'no2']:
-      df_clean[col] = pd.to_numeric(df_clean[col], errors='coerce')
-  df_clean = df_clean.dropna()
-  st.session_state.data = df_clean
-  X = df_clean.drop('kategori', axis=1)
-  y = df_clean['kategori']
-
-  #dataset mapping
-  ordinal_mapping = {
-      'BAIK': 0,
-      'SEDANG': 1,
-      'TIDAK SEHAT': 2,
-      'SANGAT TIDAK SEHAT': 3
-  }
-
-  y_encoded = y.map(ordinal_mapping)
-
-  #data splitting
-  X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, random_state=42, stratify=y)
-  st.session_state.X_train = X_train
-  st.session_state.X_test = X_test
-  st.session_state.y_train = y_train
-  st.session_state.y_test = y_test
-
-#ordinal logistic regression
-if 'ordinal_logistic' not in st.session_state:
-  from statsmodels.miscmodels.ordinal_model import OrderedModel
-  mod_log = OrderedModel(st.session_state.y_train,
-                        st.session_state.X_train,
-                        distr='logit')
-  res_log = mod_log.fit(method='bfgs', disp=False)
-  st.session_state.ordinal_logistic = res_log
-
-
-#XGBoost
-if 'xgboost' not in st.session_state:
-  import xgboost as xgb
-  xgboost = xgb.XGBClassifier(
-      objective='multi:softmax',
-      num_class=4,
-      max_depth=25,
-      learning_rate=0.001,
-      n_estimators=100,
-  )
-  xgboost.fit(st.session_state.X_train, st.session_state.y_train)
-  st.write('selesai trainning')
-  st.session_state.xgboost = xgboost
-  st.write('dah simpen')
-    
-  #tabnet
-if 'tabnet' not in st.session_state:
-  from pytorch_tabnet.tab_model import TabNetClassifier
-  tabnet = TabNetClassifier()
-  tabnet.load_model('assets/full_tabnet_model.pth.zip')
-  st.session_state.tabnet = tabnet
 
