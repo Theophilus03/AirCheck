6b27c550028d617ca78429290e84d43af77671ea
Theophilus Nathan Tanudjaja <114735443+Theophilus03@users.noreply.github.com>
2025-05-31 16:27:36 +0700

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -8,57 +8,60 @@ from sklearn.naive_bayes import GaussianNB
 import xgboost as xgb
 from pytorch_tabnet.tab_model import TabNetClassifier
 
-if 'data' not in st.session_state:
-    df = pd.read_csv('assets/final.csv')
-    df_clean = df.copy()
-    df_clean = df_clean.replace(['-', 'TIDAK ADA DATA', '---'], np.nan)
-    df_clean = df_clean[['pm10', 'so2', 'co', 'o3', 'no2', 'kategori']]
-    
-    for col in ['pm10', 'so2', 'co', 'o3', 'no2']:
-      df_clean[col] = pd.to_numeric(df_clean[col], errors='coerce')
-    df_clean = df_clean.dropna()
-    st.session_state.data = df_clean
-    X = df_clean.drop('kategori', axis=1)
-    y = df_clean['kategori']
-    
-    #dataset mapping
-    ordinal_mapping = {
-      'BAIK': 0,
-      'SEDANG': 1,
-      'TIDAK SEHAT': 2,
-      'SANGAT TIDAK SEHAT': 3
-    }
-    
-    y_encoded = y.map(ordinal_mapping)
-    
-    #data splitting
-    X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, random_state=42, stratify=y)
-    st.session_state.X_train = X_train
-    st.session_state.X_test = X_test
-    st.session_state.y_train = y_train
-    st.session_state.y_test = y_test
-#naive bayes
-if 'naive_bayes' not in st.session_state:
-    nb = GaussianNB()
-    nb.fit( st.session_state.X_train,  st.session_state.y_train)
-    st.session_state.naive_bayes = nb
-#ordinal logistic regression
-if 'ordinal_logistic' not in st.session_state:
-    mod_log = OrderedModel(st.session_state.y_train,
-                            st.session_state.X_train,
-                            distr='logit')
-    res_log = mod_log.fit(method='bfgs', disp=False)
-    st.session_state.ordinal_logistic = res_log
 
-
-#XGBoost
-if 'xgboost' not in st.session_state:
-    xgboost = xgb.XGBClassifier()
-    xgboost.load_model('assets/xgb_model.json')
-    st.session_state.xgboost = xgboost
+def load(){
+    if 'data' not in st.session_state:
+        df = pd.read_csv('assets/final.csv')
+        df_clean = df.copy()
+        df_clean = df_clean.replace(['-', 'TIDAK ADA DATA', '---'], np.nan)
+        df_clean = df_clean[['pm10', 'so2', 'co', 'o3', 'no2', 'kategori']]
+        
+        for col in ['pm10', 'so2', 'co', 'o3', 'no2']:
+          df_clean[col] = pd.to_numeric(df_clean[col], errors='coerce')
+        df_clean = df_clean.dropna()
+        st.session_state.data = df_clean
+        X = df_clean.drop('kategori', axis=1)
+        y = df_clean['kategori']
+        
+        #dataset mapping
+        ordinal_mapping = {
+          'BAIK': 0,
+          'SEDANG': 1,
+          'TIDAK SEHAT': 2,
+          'SANGAT TIDAK SEHAT': 3
+        }
+        
+        y_encoded = y.map(ordinal_mapping)
+        
+        #data splitting
+        X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, random_state=42, stratify=y)
+        st.session_state.X_train = X_train
+        st.session_state.X_test = X_test
+        st.session_state.y_train = y_train
+        st.session_state.y_test = y_test
+    #naive bayes
+    if 'naive_bayes' not in st.session_state:
+        nb = GaussianNB()
+        nb.fit( st.session_state.X_train,  st.session_state.y_train)
+        st.session_state.naive_bayes = nb
+    #ordinal logistic regression
+    if 'ordinal_logistic' not in st.session_state:
+        mod_log = OrderedModel(st.session_state.y_train,
+                                st.session_state.X_train,
+                                distr='logit')
+        res_log = mod_log.fit(method='bfgs', disp=False)
+        st.session_state.ordinal_logistic = res_log
     
-  #tabnet
-if 'tabnet' not in st.session_state:
-    tabnet = TabNetClassifier()
-    tabnet.load_model('assets/full_tabnet_model.pth.zip')
-    st.session_state.tabnet = tabnet
+    
+    #XGBoost
+    if 'xgboost' not in st.session_state:
+        xgboost = xgb.XGBClassifier()
+        xgboost.load_model('assets/xgb_model.json')
+        st.session_state.xgboost = xgboost
+        
+      #tabnet
+    if 'tabnet' not in st.session_state:
+        tabnet = TabNetClassifier()
+        tabnet.load_model('assets/full_tabnet_model.pth.zip')
+        st.session_state.tabnet = tabnet
+}
