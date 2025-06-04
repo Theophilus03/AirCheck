f84b6f23319c2655753b5f422ead95ff99b6f8f6
Theophilus Nathan Tanudjaja <114735443+Theophilus03@users.noreply.github.com>
2025-06-04 13:18:57 +0700

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -112,19 +112,13 @@ def explain_heatmap(corr_matrix):
         st.write("### Pasangan Variabel dengan Korelasi Kuat (|Korelasi| > 0.7):")
         for pair in strong_corr_pairs:
             st.write(f"Variabel {pair[0]} dan {pair[1]} memiliki korelasi kuat ({pair[2]:.2f})")
-    else:
-        st.write("Tidak ada pasangan variabel dengan |Korelasi| > 0.7")
 
     if moderate_corr_pairs:
         st.write("\n### Pasangan Variabel dengan Korelasi Sedang (0.3 < |Korelasi| < 0.7):")
         for pair in moderate_corr_pairs:
             st.write(f"Variabel {pair[0]} dan {pair[1]} memiliki korelasi sedang ({pair[2]:.2f})")
-    else:
-        st.write("Tidak ada pasangan variabel dengan (0.3 < |Korelasi| < 0.7)")
 
     if weak_corr_pairs:
         st.write("\n### Pasangan Variabel dengan Korelasi Lemah (|Korelasi| < 0.3):")
         for pair in weak_corr_pairs:
             st.write(f"Variabel {pair[0]} dan {pair[1]} memiliki korelasi lemah ({pair[2]:.2f})")
-    else:
-        st.write("Tidak ada pasangan variabel dengan |Korelasi| < 0.3")
